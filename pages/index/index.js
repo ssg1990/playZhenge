a9ea79f3fb2dffafdbe811a5b8f007194009bbba
孙曙光 <sunshuguang@jackdeiMac.local>
2018-02-23 10:08:39 +0800

    add files

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,32 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    title: '动臻格康复管家',
+    tip: '赛事康复个性化定制服务',
+    imgUrls: [
+      '/assets/img/cover1.jpeg',
+      '/assets/img/cover2.jpeg',
+      '/assets/img/cover3.jpeg',
+      '/assets/img/cover4.jpeg',
+    ],
+    indicatorDots: true,
+    circular: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000,
+    indicatorColor: "rgba(0,0,0,.3)",
+    indicatorActiveColor: "rgba(255,255,255,.7)",
+    btnSrc: '/assets/img/svg/check.svg',
+    btnText: '立即预约',
+    userSrc: '/assets/img/svg/user.svg',
+    bottomTextTips: '查看预约信息',
+    rightArrowSrc: '/assets/img/svg/rightArrow.svg'
   },
-  //事件处理函数
-  bindViewTap: function() {
+  viewOrderList() {
     wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      url: '/pages/orderList/orderList',
     })
   }
 })
+
+
---
+++ pages/orderList/orderList.js
@@ -0,0 +1,74 @@
+// pages/orderList/orderList.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    item: {
+      detailSrc: '/assets/img/svg/detail.svg',
+      mainPart: '主要部位',
+      'package': '康复套餐',
+      completed: false,
+      timeStr: '预计所需时间',
+      timeNum: '6-12 min',
+      suggest: '已完成~ 我有话要说 >>>'
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
